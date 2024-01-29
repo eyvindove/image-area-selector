e23d88c5dd511259b3de09c61fd99dbecb958fe6
Chungwei Weng <eyvindove@gmail.com>
2024-01-29 09:49:04 +0800

    build: setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,39 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [
+        react({
+          jsxImportSource: "@emotion/react",
+        }),
+      ],
+      resolve: {
+        alias: [
+          {
+            find: "@",
+            replacement: fileURLToPath(new URL("./src", import.meta.url)),
+          },
+        ],
       },
-    ],
-  },
+    };
+  } else {
+    return {
+      base: "/image-area-selector",
+      plugins: [
+        react({
+          jsxImportSource: "@emotion/react",
+        }),
+      ],
+      resolve: {
+        alias: [
+          {
+            find: "@",
+            replacement: fileURLToPath(new URL("./src", import.meta.url)),
+          },
+        ],
+      },
+    };
+  }
 });
